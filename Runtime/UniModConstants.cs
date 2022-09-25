0b28ca71236b9dc5d99feb150f93aa3f2120609d
kataS <pagumi.dev@gmail.com>
2022-09-25 18:01:16 +0200

    Updated mod file extension

--- Runtime/UniModConstants.cs
+++ Runtime/UniModConstants.cs
@@ -3,7 +3,7 @@ namespace Katas.UniMod
     public static class UniModConstants
     {
         public const string InfoFile = "info.json";
-        public const string ModFileExtensionNoDot = "mod";
+        public const string ModFileExtensionNoDot = "umod";
         public const string ModFileExtension = "." + ModFileExtensionNoDot;
         public const string CatalogName = "mod_content";
         public const string StartupAddress = "__mod_startup";
