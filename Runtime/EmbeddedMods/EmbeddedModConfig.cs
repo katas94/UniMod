f36e63383156c08a9930214571b87360d911c640
kataS <pagumi.dev@gmail.com>
2022-10-16 12:31:36 +0200

    Added headers to the EmbeddedModConfig's inspector

--- Runtime/EmbeddedMods/EmbeddedModConfig.cs
+++ Runtime/EmbeddedMods/EmbeddedModConfig.cs
@@ -6,6 +6,7 @@ namespace Katas.UniMod
     [CreateAssetMenu(fileName = "EmbeddedModConfig", menuName = "UniMod/Embedded Mod Config")]
     public sealed class EmbeddedModConfig : ScriptableObject
     {
+        [Header("Configuration")]
         public string modId;
         public string modVersion;
         public string displayName;
@@ -13,8 +14,12 @@ namespace Katas.UniMod
         public bool containsAssets;
         public ModStartup startup;
         public List<ModReference> dependencies;
+        
+        [Header("Target Application")][Space(5)]
         public string appId;
         public string appVersion;
+        
+        [Header("Includes")][Space(5)]
         public List<EmbeddedModAssemblies> assemblies = new();
     }
 }
